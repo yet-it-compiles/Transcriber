6432a55f6302d46b7c175199748c484c08acf18c
Tyler S. Unsworth <43221618+yet-it-compiles@users.noreply.github.com>
2023-04-16 21:57:24 -0700

    Ref: Refactor the widgets to increase modularity
    Refactored MetricWidgets to reduce repeated information, and changed useState to return an object

--- src/components/widgets/MetricWidgets.jsx
+++ src/components/widgets/MetricWidgets.jsx
@@ -12,46 +12,87 @@ import React, { useState, memo } from "react";
 import styles from "./metric-widgets.module.css";
 
 /**
- * Defines four memoized metric widget components for the Dashboard
+ * Defines a memoized metric widget components for the Dashboard.jsx
  *
  * The component is memoized using 'memo' since its behavior is independent of
  * its props which ensures better performance by preventing unnecessary re-renders
  *
- * @returns Four JSX metric widgets
+ * @returns {JSX.Element} - a JSX metric widget
  */
 const Widgets = memo(() => {
-	const [totalMinutesUsed, setTotalMinutesUsed] = useState(0);
-	const [numberOfRecordings, setNumberOfRecordings] = useState(0);
-	const [currentMonthBalance, setCurrentMonthBalance] = useState(0);
-	const [totalMemoryUsed, setTotalMemoryUsed] = useState(0);
+	const [metrics, setMetrics] = useState({
+		totalMinutesUsed: 0,
+		numberOfRecordings: 0,
+		currentMonthBalance: 0.0,
+		totalMemoryUsed: 0,
+		percentChange: 1,
+	});
 
 	return (
+		//className={styles.metricCardsContainer}
 		<>
-			<div className={styles.metricCard}>
-				<p>Total Minutes Used</p>
-				<p>{totalMinutesUsed} minutes</p>
-				<p>0.00%</p>
+			<div className={styles.metricCardContainer}>
+				<MetricCard
+					title="Total Minutes Used"
+					value={metrics.totalMinutesUsed}
+					unit="minutes"
+					sign={metrics.percentChange}
+				/>
 			</div>
 
-			<div className={styles.metricCard}>
-				<p>Number of Recordings</p>
-				<p>{numberOfRecordings} recordings</p>
-				<p>0.00%</p>
+			<div className={styles.metricCardContainer}>
+				<MetricCard
+					title="Number of Recordings"
+					value={metrics.numberOfRecordings}
+					unit="recordings"
+					sign={metrics.percentChange}
+				/>
 			</div>
 
-			<div className={styles.metricCard}>
-				<p>This Month's Balance</p>
-				<p>{currentMonthBalance} USD</p>
-				<p>0.00%</p>
+			<div className={styles.metricCardContainer}>
+				<MetricCard
+					title="This Month's Balance"
+					value={metrics.currentMonthBalance}
+					unit="USD"
+					sign={metrics.percentChange}
+				/>
 			</div>
-
-			<div className={styles.metricCard}>
-				<p>Total Memory Used</p>
-				<p>{totalMemoryUsed} utilized</p>
-				<p>0.00%</p>
+			<div className={styles.metricCardContainer}>
+				<MetricCard
+					title="Total Memory Used"
+					value={metrics.totalMemoryUsed}
+					unit="utilized"
+					sign={metrics.percentChange}
+				/>
 			</div>
 		</>
 	);
 });
+
+/**
+ * Renders a metric card with a title, value, unit, and sign.
+ *
+ * @param {string} title - The title of the metric card.
+ * @param {number} value - The value of the metric card.
+ * @param {string} unit - The unit of the metric card.
+ * @param {number} sign - The number of decimal places to show for the sign (percentage).
+ *
+ * @returns {JSX.Element} - A metric card component with the specified props.
+ */
+const MetricCard = ({ title, value, unit, sign }) => {
+	return (
+		(
+			// className={styles.metricCard}
+			<>
+				<h1>{title}</h1>
+				<h2>
+					{value} {unit}
+				</h2>
+				<h3>{value ? (value * 100).toFixed(sign) : "0.00"}%</h3>
+			</>
+		),
+		[title, value, unit, sign]
+	);
+};
 
 export default Widgets;
