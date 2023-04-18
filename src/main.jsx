5f90f18b57ea45032f346cf1737b5d818365599d
Tyler S. Unsworth <43221618+yet-it-compiles@users.noreply.github.com>
2023-04-18 00:01:00 -0700

    Chore: Change view

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@
  *
  * @description
  * Serves as the primary entry point component for the application tasked with
- * the responsibility to render the initial component along with hooking itself
- * into the index HTML document which allows it to asynchronously render each
- * update
+ * the responsibility to render the initial component along with hooking
+ * itself into the index HTML document which allows it to asynchronously render
+ * each update
  *
  * @requires react
  * @requires react-dom/client
@@ -15,11 +15,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Header from "./components/header/Header";
 import MetricWidgets from "./components/widgets/MetricWidgets";
+import NavigationLeft from "./components/navigation/NavigationLeft";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<App />
 		<MetricWidgets />
+		<Header />
+		<NavigationLeft />
 	</React.StrictMode>
 );
