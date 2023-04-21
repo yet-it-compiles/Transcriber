99d0c82092476b936b73264475066fb47889aeb3
yet-it-compiles <bmxfreestyle357@gmail.com>
2023-04-21 03:19:13 -0700

    Feat: Continued work

--- src/components/audio-panel/AudioRecorder.jsx
+++ src/components/audio-panel/AudioRecorder.jsx
@@ -6,18 +6,31 @@ const AudioPanel = () => {
 	const [duration, setDuration] = useState(0);
 	const [volume, setVolume] = useState(0.5);
 	const [playbackRate, setPlaybackRate] = useState(1);
+	const [isPlaying, setIsPlaying] = useState(false);
 
-	const handleDuration = (duration) => {
+	const handleDuration = useCallback((duration) => {
 		setDuration(duration);
-	};
+	}, []);
 
-	const handleVolumeChange = (event) => {
+	const handleVolumeChange = useCallback((event) => {
 		setVolume(parseFloat(event.target.value));
-	};
+	}, []);
 
-	const handlePlaybackRateChange = (event) => {
+	const handlePlaybackRateChange = useCallback((event) => {
 		setPlaybackRate(parseFloat(event.target.value));
-	};
+	}, []);
+
+	const handlePlay = useCallback(() => {
+		setIsPlaying(true);
+	}, []);
+
+	const handlePause = useCallback(() => {
+		setIsPlaying(false);
+	}, []);
+
+	const durationInSeconds = useMemo(() => {
+		return Math.floor(duration);
+	}, [duration]);
 
 	return (
 		<div className={styles.container}>
@@ -28,46 +41,38 @@ const AudioPanel = () => {
 					onDuration={handleDuration}
 					volume={volume}
 					playbackRate={playbackRate}
+					onPlay={handlePlay}
+					onPause={handlePause}
 				/>
-				<Controls
-					duration={duration}
-					volume={volume}
-					playbackRate={playbackRate}
-					onVolumeChange={handleVolumeChange}
-					onPlaybackRateChange={handlePlaybackRateChange}
-				/>
-			</div>
-		</div>
-	);
-};
+				<div className={styles.controls}>
+					<p>Duration: {durationInSeconds} seconds</p>
 
-const Controls = ({
-	duration,
-	volume,
-	playbackRate,
-	onVolumeChange,
-	onPlaybackRateChange,
-}) => {
-	return (
-		<div className={styles.controls}>
-			<div className={styles.controlGroup}>
-				<p>Duration: {duration.toFixed(2)} seconds</p>
+					<input
+						type="range"
+						id="volume"
+						name="volume"
+						min="0"
+						max="1"
+						step="0.01"
+						onChange={handleVolumeChange}
+					/>
+					<label htmlFor="playbackRate">Playback Rate:</label>
+					<select
+						id="playbackRate"
+						name="playbackRate"
+						value={playbackRate}
+						onChange={handlePlaybackRateChange}
+					>
+						<option value="0.5">0.5x</option>
+						<option value="1">1x</option>
+						<option value="1.5">1.5x</option>
+						<option value="2">2x</option>
+					</select>
+				</div>
 			</div>
-
-			<div className={styles.controlGroup}>
-				<label htmlFor="playbackRate">Playback Rate:</label>
-				<select
-					id="playbackRate"
-					name="playbackRate"
-					value={playbackRate}
-					onChange={onPlaybackRateChange}
-				>
-					<option value="0.5">0.5x</option>
-					<option value="1">1x</option>
-					<option value="1.5">1.5x</option>
-					<option value="2">2x</option>
-				</select>
-			</div>
+			<div
+				className={`${styles.waveform} ${isPlaying ? styles.playing : ""}`}
+			/>
 		</div>
 	);
 };
