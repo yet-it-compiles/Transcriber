0fe268cd63c96e36715808a7ef3bf87c506c67b6
yet-it-compiles <bmxfreestyle357@gmail.com>
2023-04-23 02:11:49 -0700

    Feat: Initial right navigation files commit

---
+++ src/components/navigation/right/RightNavigation.jsx
@@ -0,0 +1,45 @@
+/**
+ * @file RightNavigation.jsx
+ *
+ * @description
+ *
+ * @requires react
+ * @requires RightNavigation.module.css
+ *
+ * @exports RightNavigation
+ */
+
+import React, { useState } from 'react';
+import styles from './right-nav.module.css';
+
+/**
+ * Responsible for
+ *
+ * Accomplished by
+ *
+ * @returns {JSX.Element} -
+ */
+const RightNavigation = () => {
+  return (
+    <>
+      <ComponentName/>
+    </>
+  );
+};
+
+/**
+ * Responsible for
+ *
+ * Accomplished by
+ *
+ * @returns {JSX.Element} -
+ */
+const ComponentName = () => {
+  return (
+    <>
+      <h1>Welcome to RightNavigation</h1>
+    </>
+  );
+};
+
+export default RightNavigation;
