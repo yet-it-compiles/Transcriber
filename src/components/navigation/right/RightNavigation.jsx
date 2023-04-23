7ade3b0e0321ca9e64ad8237c328dddb4e156f06
yet-it-compiles <bmxfreestyle357@gmail.com>
2023-04-23 05:55:17 -0700

    Style: Style the right navbar

--- src/components/navigation/right/RightNavigation.jsx
+++ src/components/navigation/right/RightNavigation.jsx
@@ -11,6 +11,9 @@
 
 import React, { useState } from 'react';
 import styles from './right-nav.module.css';
+import { FcGoogle, FcLeave, FcTodoList} from 'react-icons/fc';
+import { MdNotificationsActive } from 'react-icons/md';
+import CalendarWidget from '../../calendar-widget/CalendarWidget';
 
 /**
  * Responsible for
@@ -21,9 +24,10 @@ import styles from './right-nav.module.css';
  */
 const RightNavigation = () => {
   return (
-    <>
-      <ComponentName/>
-    </>
+    <nav className={styles.navRight}>
+      <CalendarWidget/>
+       <Notifications/>
+    </nav>
   );
 };
 
@@ -34,11 +38,31 @@ const RightNavigation = () => {
  *
  * @returns {JSX.Element} -
  */
-const ComponentName = () => {
+const Notifications = () => {
   return (
-    <>
-      <h1>Welcome to RightNavigation</h1>
-    </>
+    <div className={styles.notifications}>
+      <p>Notifications</p>
+
+      <div>
+        <MdNotificationsActive/>
+        <p>You Have 3 Transcripts Left To Finish!</p>
+      </div>
+      <span>Just Now</span>
+
+
+
+      <div>
+        <FcLeave/>
+        <p>Your appointment just started</p>
+      </div>
+      <span><p>59 minutes ago</p></span>
+
+      <div>
+        <FcTodoList/>
+        <p>Don't forget to update your calendar for next week!</p>
+      </div>
+      <span>12 hours ago</span>
+    </div>
   );
 };
 
