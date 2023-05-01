6935c843e43c0bd0cb5db5db12ad28e91adc3661
yet-it-compiles <bmxfreestyle357@gmail.com>
2023-05-01 15:14:40 -0700

    Chore: Change

--- src/App.jsx
+++ src/App.jsx
@@ -16,20 +16,15 @@
  */
 
 import React from "react";
-//import AuthContextProvider from "./contexts/AuthContext";
-//import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LeftNavigation from "./components/navigation/left/LeftNavigation";
+/* import LeftNavigation from "./components/navigation/left/LeftNavigation";
 import DashboardHeader from "./components/dashboard-header/DashboardHeader";
 import RightNavigation from "./components/navigation/right/RightNavigation";
 import CalendarWidget from "./components/calendar-widget/CalendarWidget";
-import MetricWidgets from "./components/widgets/MetricWidgets";
+import MetricWidgets from "./components/widgets/MetricWidgets"; */
 
 /* Responsive Design Completed For Everything Above This Comment */
-import TextEditor from "./components/text-editor/TextEditor";
-import MediaRecorder from "./components/media-player/MediaRecorder";
-//import LoginScreen from "./components/login/LoginScreen";
-//import Dashboard from "./components/dashboard/Dashboard";
-//import ForgotPassword from "./components/login/ForgotPassword";
+/* import TextEditor from "./components/text-editor/TextEditor"; */
+import MediaPlayerUI from "./components/media-player/MediaPlayer";
 
 /**
  * Entry level component that renders the application
@@ -40,45 +35,18 @@ const App = () => {
     return (
         <>
             {/* 1. Dashboard View - Active */}
-
-            <RightNavigation />
+            {/* <RightNavigation />
             <DashboardHeader />
             <LeftNavigation />
-            <MetricWidgets />
+            <MetricWidgets /> */}
 
             {/* 2. Completed / Nearly Completed Pages />*/}
-
             {/* <Recording />
             <CalendarWidget />
             <TextEditor /> */}
 
             {/* 3. Functionality Testing / Building */}
-
-            {/*  <BrowserRouter>
-                <Routes>
-                    <Route
-                        path="/"
-                        element={<MediaRecorder />}
-                    />
-                    <Route
-                        path="/login"
-                        element={
-                            <AuthContextProvider>
-                                <LoginScreen />
-                            </AuthContextProvider>
-                        }
-                    />
-                    <Route
-                        path="/dashboard"
-                        element={<Dashboard />}
-                    />
-
-                    <Route
-                        path="/forgot"
-                        element={<ForgotPassword />}
-                    />
-                </Routes>
-            </BrowserRouter> */}
+            <MediaPlayerUI />
         </>
     );
 };
