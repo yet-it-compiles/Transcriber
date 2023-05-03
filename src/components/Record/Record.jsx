3e212d51540c5311adea95fe7a93418ee4697d39
yet-it-compiles <bmxfreestyle357@gmail.com>
2023-05-03 04:26:35 -0700

    : Feat: Recording page
    Recording page has conditional rendering on the mic button click generating a audio wave

--- src/components/Record/Record.jsx
+++ src/components/Record/Record.jsx
@@ -4,8 +4,6 @@
  * @description
  *
  * @requires react
- *
- * @requires react
  * @requires Record.module.css
  *
  * @exports Record
@@ -31,37 +29,71 @@ const Record = () => {
 };
 
 /**
- * Responsible for
+ * Responsible for handling the recording feature by providing the user the
+ * ability to press the record icon.
  *
- * Accomplished by
+ * Accomplished by setting up an event listener waiting for the user to click
+ * the record icon. Once the user clicks record, an audio wave animation starts
+ * playng.
  *
- * @returns {JSX.Element} -
+ * @returns {JSX.Element} - Representing text, and a single button
  */
 const ComponentName = () => {
     const [isRecording, setIsRecording] = useState(false);
+    const [showWaveAnimation, setShowWaveAnimation] = useState(false);
 
     const handleIsRecording = () => {
         setIsRecording((prev) => !prev);
+        setShowWaveAnimation((prev) => !prev);
     };
 
     return (
         <div className={styles.container}>
-            <h1>Please tap the record icon when you're ready</h1>
+            <h1 className={styles.trackingInContract}>
+                Please tap the record icon when you're ready
+            </h1>
             <button
-                className={styles.recordButton}
+                className={styles.bounceInTop}
                 type="submit"
-                onClick={handleIsRecording}
-            >
+                onClick={handleIsRecording}>
                 <span
-                    className={isRecording ? styles.recordButtonActive : null}
-                >
+                    className={isRecording ? styles.recordButtonActive : null}>
                     {isRecording ? (
-                        <CiMicrophoneOn className={styles.icons} />
-                    ) : (
                         <CiMicrophoneOff className={styles.icons} />
+                    ) : (
+                        <CiMicrophoneOn className={styles.icons} />
                     )}
                 </span>
             </button>
+
+            {showWaveAnimation && <AudioWaveAnimation />}
+
+            <div className={styles.conseal}></div>
+        </div>
+    );
+};
+
+/**
+ * Generates a wave animation that will be connected to the playback audio
+ *
+ * @returns {JSX.Element} Representing an audio wave animation
+ */
+const AudioWaveAnimation = () => {
+    const audioWaves = Array.from(
+        { length: 20 },
+        (_, eachIndex) => eachIndex + 1
+    );
+    return (
+        <div className={styles.waveContainer}>
+            {audioWaves.map((eachWave) => (
+                <span
+                    key={eachWave}
+                    style={{
+                        left: `${6 * eachWave}px`,
+                        animationDelay: `${0.3 * eachWave}s`,
+                    }}
+                />
+            ))}
         </div>
     );
 };
