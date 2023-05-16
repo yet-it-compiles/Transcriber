b935433718dadd30966bb19517f8b5bcd7a72757
yet-it-compiles <bmxfreestyle357@gmail.com>
2023-05-15 23:23:00 -0700

    📄 Docs: Update dashboard documentation

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,26 +1,40 @@
 /**
  * @file Dashboard.jsx
  *
- * @description
+ * @description This component is responsible for rendering the content on the
+ * dashboard.
  *
  * @requires react
+ * @requires modal.module.scss
  *
  * @exports Dashboard
  */
 
 import React from "react";
-import LeftNavigation from "../../components/navigation/left/LeftNavigation";
-import RightNavigation from "../../components/navigation/right/RightNavigation";
-import DashboardHeader from "../../components/header/Header";
+import DisplayModal from "../../components/modal/DisplayModal";
 
-function Dashboard() {
+const Dashboard = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <>
-      <RightNavigation />
-      <DashboardHeader />
-      <LeftNavigation />
+      <h2> Quick Access </h2>
+      {/* Define Modals */}
+      <div>
+        <button onClick={() => setIsModalOpen(true)}>Clients</button>
+        <button>Transcripts</button>
+        <button>Recordings</button>
+        <button>Current Editor</button>
+        <button>Downloads</button>
+        <button>Recently Deleted</button>
+        <DisplayModal isOpen={isModalOpen} />
+      </div>
+
+      <h2>Previews</h2>
+
+      <h2>Recent Files</h2>
     </>
   );
-}
+};
 
 export default Dashboard;
