6b0d4e95036fcec7b9f82b0ded768c145398167f
TannerSlick <tanners101@yahoo.com>
2023-05-17 23:57:47 -0700

    Re-added Register routing, updated comments, uncommented login/password but swapped order to still contain Home page first

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,14 @@ import AuthContextProvider from "./context/AuthContext";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/home/Home";
-/* import Login from "./pages/login/Login";
-import ForgotPassword from "./pages/forgot-password/ForgotPassword"; */
+import Login from "./pages/login/Login";
+import ForgotPassword from "./pages/forgot-password/ForgotPassword";
+import Register from "./pages/register/Register"
 import StartRecording from "./pages/start-recording/StartRecording";
 import EditTranscript from "./pages/editor/EditTranscript";
 import DisplayAnalytics from "./pages/display-analytics/DisplayAnalytics";
 import SupportFAQ from "./pages/support-FAQ/SupportFAQ";
+
 
 /**
  * Entry level component that renders the application
@@ -30,9 +32,10 @@ const App = () => {
     <BrowserRouter>
       <AuthContextProvider>
         <Routes>
-          {/* <Route path="/" element={<Login />} /> */}
-          {/* <Route path="/forgot-password" element={<ForgotPassword />} /> */}
           <Route path="/" element={<Home />} />
+          <Route path="/" element={<Login />} /> 
+          <Route path="/forgot-password" element={<ForgotPassword />} /> 
+          <Route path="/register-account" element={<Register/>}/>
           <Route path="/start-recording" element={<StartRecording />} />
           <Route path="/editor" element={<EditTranscript />} />
           <Route path="/display-analytics" element={<DisplayAnalytics />} />
