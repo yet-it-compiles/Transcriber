a87c5dd6b8bfda48146dc33d954ca522195349b0
yet-it-compiles <bmxfreestyle357@gmail.com>
2023-05-23 00:39:32 -0700

    ⚙️Perf: Improved UX and functionality

--- src/components/uploader/Uploader.jsx
+++ src/components/uploader/Uploader.jsx
@@ -40,6 +40,9 @@ const Uploader = () => {
     isImage: false,
     isAudio: false,
     isTranscript: false,
+    fontStyle: "Arial",
+    fontColor: "black",
+    backgroundColor: "white",
   });
 
   /**
@@ -72,11 +75,18 @@ const Uploader = () => {
           isLoading: false,
         }));
       };
-
       const isImage = documentState.file.type.startsWith("image");
-      setDocumentState((prevState) => ({ ...prevState, isImage }));
+      const isAudio = documentState.file.type.startsWith("audio");
+      const isTranscript = documentState.file.type === "text/plain";
 
-      if (isImage) {
+      setDocumentState((prevState) => ({
+        ...prevState,
+        isImage,
+        isAudio,
+        isTranscript,
+      }));
+
+      if (isImage || isAudio) {
         reader.readAsDataURL(documentState.file);
       } else {
         reader.readAsText(documentState.file);
@@ -97,13 +107,81 @@ const Uploader = () => {
     setDocumentState((prevState) => ({ ...prevState, file: newFile }));
   };
 
+  /**
+   * @setStateFunction handleDragDrop
+   *
+   * @param {File Object} newDocument
+   */
+  const handleDragDrop = (newDocument) => {
+    newDocument.preventDefault();
+
+    const file = newDocument.dataTransfer.files[0];
+    setDocumentState((prevState) => ({ ...prevState, file }));
+  };
+
+  const handleEscape = () => {
+    setDocumentState({
+      file: null,
+      content: null,
+      error: null,
+      isLoading: false,
+      isImage: false,
+      isAudio: false,
+      isTranscript: false,
+      fontStyle: "Arial",
+      fontColor: "black",
+      backgroundColor: "white",
+    });
+  };
+
+  /**
+   * @setStateFunction handleFontStyleChange
+   *
+   * @description State updater function that handles updating the newly selected
+   * font style
+   */
+  const handleFontStyleChange = (event) => {
+    const fontStyle = event.target.value;
+    setDocumentState((prevState) => ({ ...prevState, fontStyle }));
+  };
+
+  /**
+   * @setStateFunction handleFontColorChange
+   *
+   * @description State updater function that handles updating the newly selected
+   * font color style
+   */
+  const handleFontColorChange = (event) => {
+    const fontColor = event.target.value;
+    setDocumentState((prevState) => ({ ...prevState, fontColor }));
+  };
+
+  /**
+   * @setStateFunction handleBackgroundColorChange
+   *
+   * @description State updater function that handles updating the newly selected
+   * background color change
+   */
+  const handleBackgroundColorChange = (event) => {
+    const backgroundColor = event.target.value;
+    setDocumentState((prevState) => ({ ...prevState, backgroundColor }));
+  };
+
   return (
     <section
       role="region"
       aria-labelledby="fileUploadSection"
       className={styles.container}
+      onDrop={handleDragDrop}
+      onDragOver={(dragEvent) => dragEvent.preventDefault()}
     >
-      <h2 className={styles.title}>Upload your file</h2>
+      <button onClick={handleEscape} className={styles.backButton}>
+        🔙 Back
+      </button>
+
+      <h2 className={styles.title}>
+        🎙️ Get Started: Upload Your Audio File or Transcript To Begin 📜
+      </h2>
 
       <div className={styles.inputContainer}>
         <input
@@ -115,13 +193,48 @@ const Uploader = () => {
         />
 
         <label htmlFor="fileInput" className={styles.fileInputLabel}>
-          Select a file
+          📁 Choose Your File or Drag and Drop It Here!
         </label>
       </div>
+
+      {documentState.isTranscript && (
+        <div className={styles.transcriptOptions}>
+          <h3>Customize Transcript:</h3>
+          <div>
+            <label>Font Style:</label>
+            <select
+              value={documentState.fontStyle}
+              onChange={handleFontStyleChange}
+            >
+              <option value="Arial">Arial</option>
+              <option value="Times New Roman">Times New Roman</option>
+              <option value="Verdana">Verdana</option>
+              {/* Add more font options */}
+            </select>
+          </div>
+          <div>
+            <label>Font Color:</label>
+            <input
+              type="color"
+              value={documentState.fontColor}
+              onChange={handleFontColorChange}
+            />
+          </div>
+          <div>
+            <label>Background Color:</label>
+            <input
+              type="color"
+              value={documentState.backgroundColor}
+              onChange={handleBackgroundColorChange}
+            />
+          </div>
+        </div>
+      )}
+
       {documentState.isLoading ? (
         <div className={styles.loadingContainer}>
           <div className={styles.spinner}></div>
-          <p>Loading...</p>
+          <p>⏳ Just a moment... We're bringing in your file! ⏳</p>
         </div>
       ) : documentState.isImage && documentState.content ? (
         <img
@@ -129,13 +242,28 @@ const Uploader = () => {
           alt="Preview"
           className={styles.imagePreview}
         />
+      ) : documentState.isAudio && documentState.content ? (
+        <audio controls className={styles.audioPreview}>
+          <source src={documentState.content} type={documentState.file.type} />
+          📢 Hmm, looks like your browser isn't jamming with us. Try another
+          one? 📢
+        </audio>
       ) : (
-        <pre className={styles.fileContent}>{documentState.content}</pre>
+        <pre
+          className={styles.fileContent}
+          style={{
+            fontFamily: documentState.fontStyle,
+            color: documentState.fontColor,
+            backgroundColor: documentState.backgroundColor,
+          }}
+        >
+          {documentState.content}
+        </pre>
       )}
 
       {documentState.error && (
         <div aria-label="Error message" className={styles.error}>
-          Error: {documentState.error}
+          🚫 Oops! An error occurred: {documentState.error} 🚫
         </div>
       )}
     </section>
